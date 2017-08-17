052c23f7b758d06a47ed0f4d931d47c95c861afa
lorenzo-pc <lorenzo-pc@lorenzo-pc>
2017-08-17 18:38:29 +0200

    created basic world class (empty)

---
+++ src/tmiengine/library/world/TMIworld.java
@@ -0,0 +1,37 @@
+package tmiengine.library.world;
+
+import tmiengine.library.world.worldInterfaces.TMIworldInterface;
+import tmiengine.library.world.worldInterfaces.TMIworldMode;
+
+public class TMIworld implements TMIworldInterface {
+
+	@Override
+	public void setWorldGravity(float Gx, float Gy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateAndDraw() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void selectWorldMode(TMIworldMode mode) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void addToWorld(String object, float X, float Y, int rotation, boolean isFlipped) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeFromWorld(Object object) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/tmiengine/library/world/worldInterfaces/TMIworldMode.java
+++ src/tmiengine/library/world/worldInterfaces/TMIworldMode.java
@@ -1,9 +1,10 @@
 package tmiengine.library.world.worldInterfaces;
 
+//defines available world modes
 public enum TMIworldMode
 {
-	freeze,
-	edit,
-	simulate,
-	victory;
+	freeze, //world is stopped
+	edit, //world is under editing from the user
+	simulate, //world is simulating...
+	victory; //object score has been accomplished, world is freezed again
 }
