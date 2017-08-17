1e70c8c5b7ad1e61e8c1231340a554a57fcb15ea
lorenzo-pc <lorenzo-pc@lorenzo-pc>
2017-08-17 17:36:54 +0200

    update

---
+++ src/tmiengine/library/objects/testObject.java
@@ -0,0 +1,91 @@
+package tmiengine.library.objects;
+
+import tmiengine.library.objects.objectsInterfaces.TMIobjectsInterface;
+import fisica.FBox;
+
+class testObject extends FBox implements TMIobjectsInterface {
+
+	testObject()
+	{
+		super(40, 40);
+	}
+	
+	@Override
+	public void freeze() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unfreeze() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int update() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void changeStatus(int statusCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moveTo(float X, float Y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAsStaticBody() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAsDynamicBody() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rotateOnce() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reflect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void manipulate(float W, float H) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean checkForOverlap() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
